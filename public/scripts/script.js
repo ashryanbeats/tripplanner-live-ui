78e36f09f6f5452c374dded731cb4d133abab9e3
Ash Ryan <ash@ashryanbeats.com>
2015-03-26 14:51:58 -0400

    Itinerary list can be added to.

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -3,7 +3,19 @@ $(".add").on('click', function(){
 	var $category = $addButton.attr('id');
 	var $place = $addButton.prev().find('option:selected').text();
 	// console.log($("select option:selected").text());
-	console.log("addButton: ", $addButton);
-	console.log("category: ", $category);
-	console.log("place: ", $place);
+	// console.log("addButton: ", $addButton);
+	// console.log("category: ", $category);
+	// console.log("place: ", $place);
+
+	var $itineraryItem = '<div class="itinerary-item"><span class="title">' + $place + '</span><button class="btn btn-xs btn-danger remove btn-circle">x</button></div>';
+
+	var $listCategory = "#" + $category + "-list";
+	console.log($listCategory);
+
+
+	// find the category in the list
+	// append $itineraryItem
+	$($listCategory).append($itineraryItem);
+
+
 });
